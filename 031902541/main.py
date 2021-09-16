bab038cbc5282821a700987bd4604f7b5eb73925
smallgrape14 <60038680+smallgrape14@users.noreply.github.com>
2021-09-16 14:11:11 +0800

    最终版2.0

--- 031902541/main.py
+++ 031902541/main.py
@@ -1,38 +1,29 @@
 # -*- coding:utf-8 -*-
-#拆分左右结构的汉字，还未实现，命令行执行文件传参还没实现
+import sys
 import pypinyin
 import time
-import math
-from Pinyin2Hanzi import DefaultDagParams
-from Pinyin2Hanzi import dag
-import filecmp 
-time1 = time.time()
 
-# DFA算法
+#------DFA算法
 class DFAFilter(object):
-    def __init__(self):
+    #------初始化
+    def __init__(self):         
         self.keyword_chains = {}  # 关键词链表
         self.delimit = '\x00'  # 限定
         
-    def sums(self,index):
-        summ=0;
-        for i in range(1,index+1):
-            summ=summ+int(math.pow(3,i))
-        return summ
-      
+    #------将变形组合的敏感词加入敏感词链      
     def add(self, keyword , real_word):
-        keyword = keyword.lower()  # 关键词英文变为小写
-        chars = keyword.strip()    # 关键字去除首尾空格和换行
-        if not chars:              # 如果关键词为空直接返回
+        keyword = keyword.lower()  # 敏感词英文变为小写
+        chars = keyword.strip()    # 敏感词去除首尾空格和换行
+        if not chars:              # 如果敏感词为空直接返回
             return
         level = self.keyword_chains
-                                   # 遍历关键字的每个字
+                                   # 遍历敏感字的每个字
         for i in range(len(chars)):
-                                   # 如果这个字已经存在字符链的key中就进入其子字典
+                                   # 如果这个敏感词已经存在字符链的key中就进入其子字典
             if chars[i] in level:
                 level = level[chars[i]]
             else:
-                if not isinstance(level, dict):#如果level不是字典跳出for循环
+                if not isinstance(level, dict): #如果level不是字典跳出for循环
                     break
                 for j in range(i, len(chars)):
                     level[chars[j]] = {}
@@ -43,37 +34,34 @@ class DFAFilter(object):
         if i == len(chars) - 1:
             level[self.delimit] =real_word
             
-    def parse(self, path):     
+    #--------对原敏感词的变形组合：ex:法--fa--f--氵去 or 功--gong--g--工力--工（y与功同音）  
+    def transhape(self, path,word_split):     
         with open(path, encoding='utf-8') as f:
-            for keyword in f:#添加关键词入字典链，关键词去除首尾的符号
+            for keyword in f:               #添加敏感词入字典链，敏感词去除首尾的符号
                 keyword=keyword.strip()
                 if '\u4e00' <= keyword <= '\u9fa5':
                     lens=len(keyword)
                     ls=[]
                     for i in range(0,lens):
                         if i==0:
+                            lb=0                #左边界
                             ls.append(keyword[i])
                             ls.append(str(pinyin(keyword[i])))
                             ls.append(str(pinyin(keyword[i]))[0])
-                        elif i==1:
-                            for j in range(0,3):
-                                char=ls[j]
-                                char=char+keyword[i]
-                                ls.append(char)
-
-                                char=ls[j]
-                                char=char+str(pinyin(keyword[i]))
-                                ls.append(char)
-
-                                char=ls[j]
-                                char=char+str(pinyin(keyword[i]))[0]
-                                ls.append(char)  
-                        else:
-                            lb=self.sums(i-1)
-                            rb=lb+int(math.pow(3,i))
+                            rb=3                #右边界
+                            if keyword[i] in word_split:
+                                if pinyin(word_split[keyword[i]][0])==pinyin(keyword[i]):#左右结构的偏旁刚好又是和该字是同音字
+                                    ls.append(word_split[keyword[i]][0])
+                                    rb=rb+1
+                                ls.append(word_split[keyword[i]])
+                                rb=rb+1
+                        else:                            
+                            cnt=0
                             for j in range(lb,rb):
+                                
                                 char=ls[j]
                                 char=char+keyword[i]
+                                
                                 ls.append(char)
 
                                 char=ls[j]
@@ -83,36 +71,48 @@ class DFAFilter(object):
                                 char=ls[j]
                                 char=char+str(pinyin(keyword[i]))[0]
                                 ls.append(char)
-                    lb=self.sums(lens-1)
-                    rb=lb+int(math.pow(3,lens))                             
+                                
+                                cnt=cnt+3
+                                if keyword[i] in word_split:
+                                    if pinyin(word_split[keyword[i]][0])==pinyin(keyword[i]):#左右结构的偏旁刚好又是和该字是同音字
+                                        char=ls[j]
+                                        char=char+word_split[keyword[i]][0]
+                                        ls.append(char)
+                                        cnt=cnt+1
+                                    char=ls[j]
+                                    char=char+word_split[keyword[i]]
+                                    ls.append(char)
+                                    cnt=cnt+1
+                            lb=rb
+                            rb=rb+cnt                         
                     for i in range(lb,rb):
-                        self.add(str(ls[i]).strip(),keyword)#一个参数是真实的敏感词
+                        self.add(str(ls[i]).strip(),keyword)   #keyword是真实的敏感词
                 else:
-                    self.add(str(keyword).strip(),keyword) #00000
- 
-    def filter(self, text, repl="*"):
-        whole_text=text.read()
+                    self.add(str(keyword).strip(),keyword) 
+                    
+    #--------遍历待检测文本，检测敏感词，计算敏感词数量，定位敏感词出现的位置                  
+    def detecting(self, text):
+        whole_text=text.read()              #全文
         text.seek(0)
-        #whole_text=text.read().lower() #此处补充一个全文行数的计算函数
+        #whole_text=text.read().lower()     #此处补充一个全文行数的计算函数
         level=self.keyword_chains
-        start=0   #检索文本的索引
-        ret=[]    #检测结果的内容列表
-        lines=1  #总行数
+        start=0                             #检索文本的索引
+        ret=[]                              #检测结果的内容列表
+        lines=1                             #总行数
         exlis="1234567890"
-        posi=[]#每行尾巴的下标
-        line=text.readline()
+        posi=[]                             #文本每行右边界的下标
+        line=text.readline()                #再按行读取，来确定当前敏感词所在行
         posi.append(len(line)-1)
-        
         while start < len(whole_text):
             level = self.keyword_chains
-            step_ins = 0 #敏感词的长度
-            rl_word="" #真正的敏感词
-            fk_word="" #伪装的敏感词
-            flag=0     #是否有出现中文
-            fnum=0     #是否出现数字
-            interval=[]#敏感词每个字出现在文本中的位置
+            step_ins = 0                    #敏感词的长度
+            rl_word=""                      #真正的敏感词
+            fk_word=""                      #伪装的敏感词
+            flag=0                          #是否有出现中文
+            fnum=0                          #是否出现数字
+            interval=[]                     #敏感词每个字出现在待检测文本中的位置
             index=start
-            first=0
+            first=0                         #是否是敏感词的第一个字
             while(start>posi[-1]):
                 line=text.readline()
                 if not line:
@@ -123,24 +123,44 @@ class DFAFilter(object):
             for char in whole_text[start:]:
                 
                 if char in level or char.lower() in level:#涵盖中文的拼音，汉字，首写字母，左右结构拆分，以及正常的英文敏感词
-                    if len(interval)>0 and index-interval[-1]>20:
-                          break
+                    if len(interval)>0 and index-interval[-1]-1>20:
+                         break
                     first=1
                     real_char=char
                     if char.lower() in level:
                         real_char=char.lower()
-                    
-                    interval.append(index)
-                    if '\u4e00' <=char<= '\u9fa5':
-                        flag=1                        
                     step_ins += 1
-                    #rl_word=rl_word+char
+                    interval.append(index)
+                    if '\u4e00' <=char<= '\u9fa5':          #说明出现中文
+                        flag=1                        
                     fk_word=fk_word+char
                     if self.delimit not in level[real_char]:
                         level = level[real_char]
                     else:
                         level=level[real_char]
-                        if len(level[self.delimit])!=0 and '\u4e00' <= level[self.delimit]<= '\u9fa5':#说明该词属于中文敏感词，又已知中文敏感词不可以插入数字
+                        #处理特殊情况-----falung---->falungong取后者
+                        you=0                               #是否有更优选
+                        xu_word=""
+                        for k in whole_text[start+step_ins:]:
+                            if k in level:
+                                xu_word=xu_word+k
+                                if self.delimit not in level[k]:
+                                    level = level[k]
+                                else:
+                                    level = level[k]
+                                    you=1
+                                    rl_word=level[self.delimit]
+                                    s=""
+                                    s="Line"+str(lines)+": <"+rl_word+"> "+fk_word+xu_word
+                                    ret.append(s)
+                                    start += step_ins - 1
+                                    break
+                            else:
+                                break
+                        if you==1:
+                            break
+                        #------没有特殊情况，则正常执行
+                        if len(level[self.delimit])!=0 and '\u4e00' <= level[self.delimit]<= '\u9fa5':#说明该词属于中文敏感词，又已知插入数字的中文敏感词无效
                             if fnum==1:
                                   break
                         rl_word=level[self.delimit]
@@ -149,11 +169,10 @@ class DFAFilter(object):
                         ret.append(s)
                         start += step_ins - 1
                         break
-                elif '\u4e00' <=char<= '\u9fa5':#同音字，繁体字的出现
-                    #d=self.keyword_chains
+                elif '\u4e00' <=char<= '\u9fa5':#处理同音字，繁体字的情况
                     py_char=pinyin(char)
-                    exist=0  #是否是同音字或是繁体字
-                    rl_char=""#检测字符对应在敏感词库中的同音字或是繁体字
+                    exist=0                     #是否是同音字或是繁体字
+                    rl_char=""                  #检测字符对应在敏感词库中的同音字或是繁体字
                     for i in level.keys():
                         if py_char==pinyin(i):
                             rl_char=i
@@ -206,20 +225,37 @@ def pinyin(word):
 
 
 if __name__ == "__main__":
-    gfw = DFAFilter()
-    path = "C://Users//duan//Desktop//words.txt"
-    gfw.parse(path)
-    
-    text=open("C://Users//duan//Desktop//org.txt", "r", encoding="utf-8")
-    #text=open("C://Users//duan//Desktop//org.txt", "r", encoding="utf-8").readlines()
+    #time1 = time.time()
+    #----------命令行传参
+    if len(sys.argv) == 1:
+        be_detected_file = "C://Users//duan//Desktop//org.txt"#"C://Users//duan//Desktop//test.txt"
+        reference_file= "C://Users//duan//Desktop//words.txt"
+        result_file = "C://Users//duan//Desktop//00.txt"
+    elif len(sys.argv) == 4:
+        reference_file = sys.argv[1]   #参照文本（敏感词）
+        be_detected_file = sys.argv[2] #待检测文本
+        result_file = sys.argv[3]      #结果文本
+    else:
+        print("命令行错误")
+        exit(0)
+        
+    #------生成拆分汉字左右结构的词链    
+    split_file=open("wordsplit.txt", "r", encoding="utf-8").read()
+    word_split={}                       #拆分的字典，键为 被拆分汉字，值为拆分出来的左右结构
+    for char in split_file.split():
+        word_split[char[0]]=char[1:]
+        
+    #-------生成敏感词链      
+    test_tool = DFAFilter()             #生成测试工具对象
+    test_tool.transhape(reference_file,word_split)
 
-    result = gfw.filter(text)
-    text2=open("C://Users//duan//Desktop//00.txt", "w", encoding="utf-8").write(result)
-    ans=open("C://Users//duan//Desktop//ans.txt", "r", encoding="utf-8")
-    #print(filecmp.cmp(r'C://Users//duan//Desktop//ans.txt',r'C://Users//duan//Desktop//00.txt'))
-    print(result)
+    #-------开始对待检测文本进行检测
+    text=open(be_detected_file, "r", encoding="utf-8")
+    result = test_tool.detecting(text)
     
-    time2 = time.time()
-    print('总共耗时：' + str(time2 - time1) + 's')
+    #-------将检测结果定向输出到指定路径下
+    open(result_file, "w", encoding="utf-8").write(result)
+    #print(result)
+    
 
 
